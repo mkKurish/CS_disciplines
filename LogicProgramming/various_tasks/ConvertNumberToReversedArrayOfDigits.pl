f2d3a12f6f8fb1f851930f846fb5012201a2a156
mkKurish <kurish04@mail.ru>
2024-03-13 20:23:38 +0300

    Added description

--- LogicProgramming/various_tasks/ConvertNumberToReversedArrayOfDigits.pl
+++ LogicProgramming/various_tasks/ConvertNumberToReversedArrayOfDigits.pl
@@ -1,3 +1,11 @@
+% Given a random non-negative number, 
+% you have to return the digits of this number
+% within an array in reverse order.
+
+% Example(Input => Output):
+% 35231 => [1,3,2,5,3]
+% 0 => [0]
+
 digitize(N, Res) :- 
   N < 10,
   Res = [N]
